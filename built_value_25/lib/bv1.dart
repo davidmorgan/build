d0967bb0664c8ac7e1f8dc14ef72ecf1f9ddf68b
David Morgan <davidmorgan@google.com>
2025-01-23 12:50:05 +0100

    Investigate asset IDs. Minor fix: don't copy assetsRead if unusedAssets is empty.

--- built_value_25/lib/bv1.dart
+++ built_value_25/lib/bv1.dart
@@ -5,6 +5,8 @@ import 'app.dart';
 
 part 'bv1.g.dart';
 
+// CACHE_BUSTER941179130229
+
 abstract class Value implements Built<Value, ValueBuilder> {
   Value._();
   factory Value(void Function(ValueBuilder) updates) = _$Value;
